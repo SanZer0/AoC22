c47a5005c7f5cc484860c10522c32cbf65a51af6
Jonas Neutzner <jonas.neutzner@gmx.de>
2022-12-15 12:34:49 +0100

    Finished day 1

--- Day 1 - Calorie Counting/src/standard/Main.java
+++ Day 1 - Calorie Counting/src/standard/Main.java
@@ -21,16 +21,26 @@ public class Main {
 		Scanner sc = new Scanner(file);
 		int elfCounter = 1;
 		int calorieCounter = 0;
-		int mostCalories = 0;
-		int elfToAsk;
+		int topElves = 3;
+		int[] mostCalories = new int[topElves];
+		int[] elfToAsk = new int[topElves];
 		while(sc.hasNextLine()) {
 			//empty line means new elf
 			String line = sc.nextLine().trim();
 			if(line.isEmpty()) {
 				//set the new elfToAsk if we found one with the most calories
-				if(calorieCounter > mostCalories) {
-					elfToAsk = elfCounter;
-					mostCalories = calorieCounter;
+				for (int i = 0; i < topElves; i++) {
+					if (calorieCounter > mostCalories[i]) {
+						//shift down the list after i
+						for (int j = topElves - 1; j > i; j--) {
+							elfToAsk[j] = elfToAsk[j - 1];
+							mostCalories[j] = mostCalories[j - 1];
+						}
+						//change value from i'th elf
+						elfToAsk[i] = elfCounter;
+						mostCalories[i] = calorieCounter;
+						break;
+					}
 				}
 				elfCounter++;
 				calorieCounter = 0;
@@ -39,7 +49,13 @@ public class Main {
 				calorieCounter += Integer.parseInt(line);
 			}
 		}
-		System.out.println("Elf " + elfCounter + " has the most candies with " + mostCalories + " kcal.");
+		int combinedCalories = 0;
+		//print the top elves and accumulate the kcal
+		for (int i = 0; i < topElves; i++) {
+			System.out.println("The " + (i + 1) + ". elf to ask is #" + elfToAsk[i] + " with " + mostCalories[i] + " kcal.");
+			combinedCalories += mostCalories[i];
+		}
+		System.out.println("The Top " + topElves + " elves have " + combinedCalories + " calories with them.");
 	}
 
 }
