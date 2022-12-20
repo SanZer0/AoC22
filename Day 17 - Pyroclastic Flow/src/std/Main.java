99f1f954d43023e17b4c811c3c5ccfb9656b14d4
SanZer0 <jonas.neutzner@gmx.de>
2022-12-20 01:42:45 +0100

    Day 17 part two finished - great cycle detection lul

--- Day 17 - Pyroclastic Flow/src/std/Main.java
+++ Day 17 - Pyroclastic Flow/src/std/Main.java
@@ -6,8 +6,10 @@ package std;
 import java.awt.Point;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.math.BigInteger;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -21,12 +23,14 @@ public class Main {
 	final static String input = "input.txt";
 	static int nextWind = 0;
 	static int nextRock = 0;
-	static int highestRock = 0;
+	static long deletedLines = 0;
+	static int highestRelativeRock = 0;
 	final static int above = 3 + 1;//something needs to be cleaned again
 	final static int right = 2;
 	final static int width = 7;
 	static List<String> chamber;
 	static Rock currentRock;
+	static long counter;
 	/**
 	 * @param args
 	 * @throws FileNotFoundException 
@@ -37,19 +41,21 @@ public class Main {
 		//initialize the chamber
 		chamber = new ArrayList<String>();
 		chamber.add("+" + "-".repeat(width) + "+");
-		int counter = -1;
-		while(counter < 2022) {
+		counter = -1;
+		while(counter < 1000000000000L) {
 			if(stopped) {
+				if(counter % 10000 == 0 && counter != 0) {
+					System.out.println(counter);
+				}
 				stopped = false;
 				counter++;
 				spawnRock();
-				System.out.println(currentRock.x);
 			}
 			blowRock();
 			fallRock();
 		}
 		visualizeChamber();
-		System.out.println("Highest block is at " + highestRock);
+		System.out.println("Highest block is at " + (deletedLines + highestRelativeRock));
 
 	}
 	//write the input into the variable
@@ -66,7 +72,7 @@ public class Main {
 	//spawn the next rock
 	public static void spawnRock() {
 		//spawn a new rock
-		currentRock = new Rock(nextRock, right, highestRock + above);
+		currentRock = new Rock(nextRock, right, highestRelativeRock + above);
 		nextRock = (nextRock + 1) % 5;
 		//that's it actually
 	}
@@ -85,7 +91,7 @@ public class Main {
 			//check for rocks
 			for(Point p: coords) {
 				//get the coordinate after the change, check if #
-				if(p.y < 0 || p.y > highestRock) {continue;}
+				if(p.y < 0 || p.y > highestRelativeRock) {continue;}
 				if(chamber.get(p.y).charAt(p.x) == '#') { //next block is a rock
 					return;
 				}
@@ -95,7 +101,7 @@ public class Main {
 			//check for rocks
 			for(Point p: coords) {
 				//get the coordinate after the change, check if #
-				if(p.y < 0 || p.y > highestRock) {continue;}
+				if(p.y < 0 || p.y > highestRelativeRock) {continue;}
 				if(chamber.get(p.y).charAt(p.x+2) == '#') { //next block is a rock
 					return;
 				}
@@ -113,7 +119,7 @@ public class Main {
 		//check if it can fall
 		for(Point p: currentRock.getCoords()) {
 			//get the coordinate after the change, check for #
-			if(p.y - 1 < 0 || p.y - 1 > highestRock) {continue;}
+			if(p.y - 1 < 0 || p.y - 1 > highestRelativeRock) {continue;}
 			char nextBlock = chamber.get(p.y - 1).charAt(p.x + 1);//go down, one to the right to acc. for barrier
 			if(nextBlock == '#' || nextBlock == '-') { //next block is a rock
 				stopped = true;
@@ -128,7 +134,7 @@ public class Main {
 	}
 	
 	public static void updateChamber(Rock rock) {
-		for(int i = highestRock; i < rock.yRange()[1]; i++) {
+		for(int i = highestRelativeRock; i < rock.yRange()[1]; i++) {
 			chamber.add("|" + ".".repeat(width) + "|");
 		}
 		//pretty inefficient, but will work
@@ -137,9 +143,46 @@ public class Main {
 			arr[p.x + 1] = '#'; 
 			chamber.set(p.y, String.valueOf(arr));
 		}
-		highestRock = chamber.size() - 1;
+		highestRelativeRock = chamber.size() - 1;
+		deleteLines();
 	}
 	
+	static List<String> cycleDect = null;
+	static HashMap<String, Tuple<BigInteger, BigInteger>> map = null;
+	private static void deleteLines() {
+		if(cycleDect == null) {
+			cycleDect = new ArrayList<String>();			
+		}
+		if(map == null) {
+			map = new HashMap<String, Tuple<BigInteger, BigInteger>>();			
+		}
+		for(int i = 1; i < chamber.size(); i++) {
+			if(chamber.get(i).equals("|" + "#".repeat(width) + "|")) { //line is full
+				deletedLines += i;
+				chamber = chamber.subList(i, chamber.size());
+				highestRelativeRock -= i;
+				//System.out.println("h:"+highestRelativeRock + " s:" + chamber.size() + " r:" +nextRock + " w:" + nextWind + " d:" + deletedLines + " c:" + counter);
+				String cycleString = "h:"+highestRelativeRock + " s:" + chamber.size() + " r:" +nextRock + " w:" + nextWind;
+				//cycle detection
+				if(!cycleDect.contains(cycleString)) {
+					cycleDect.add(cycleString);
+					map.put(cycleString, new Tuple<BigInteger, BigInteger>(BigInteger.valueOf(deletedLines), BigInteger.valueOf(counter)));
+				} else {
+					long droppedRocks = counter - map.get(cycleString).y.longValue();
+					long addedHeight = deletedLines - map.get(cycleString).x.longValue();
+					//Mathe und Ende
+					System.out.println("Im Cycle werden "+ droppedRocks + " Steine gedropped und es wird "+ addedHeight + " an Höhe addiert.");
+					long remRocks = 1000000000000L - counter;
+					long cyclesLeft = remRocks / droppedRocks;
+					counter += cyclesLeft * droppedRocks;
+					deletedLines += cyclesLeft * addedHeight;
+					System.out.println("found a cycle");
+					cycleDect = null;
+				}
+				break;
+			}			
+		}
+	}
 	public static void visualizeChamber() {
 		for(int i = chamber.size()-1; i >= 0; i--) {
 			System.out.println(chamber.get(i) + i);
